be4c2a1e68f0f80a0c23e4a7227312b85da7aad7
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-02-20 20:07:36 +0200

    Update Output.hpp

--- src/Output.hpp
+++ src/Output.hpp
@@ -6,8 +6,23 @@ using namespace std;
 class Output{
 public:
 	/* all of our data attributes */
+	std::vector<int> libraries;
+	std::vector<std::vector<int>> books;
 	Output();
-	void write_to_file();
+	void write_to_file(){
+		std::cout<<libraries.size() << std::endl;;
+		for(int i = 0; i < libraries.size(); i++){
+			std::cout<<libraries[i] << " " << books[i].size() << std::endl;
+			std::copy(books[i].begin(),books[i].end(),std::ostream_iterator<int>(std::cout," "));
+			std::cout<< std::endl;
+			
+			
+			
+			
+		}
+		
+		
+	};
 
 };
 
